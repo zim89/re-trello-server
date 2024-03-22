61b86d855e2fac9a065135fef2fd41da65d52de6
zim89 <121241841+zim89@users.noreply.github.com>
2024-03-22 13:45:25 +0200

    user module

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
 import { AuthDto } from '../auth/dto/auth.dto'
 import { hash } from 'argon2'
+import { UserDto } from './dto/user.dto'
+import { startOfDay, subDays } from 'date-fns'
 
 @Injectable()
 export class UserService {
@@ -36,5 +38,73 @@ export class UserService {
     return this.prisma.user.create({
       data: user,
     })
+  }
+
+  async update(id: string, dto: UserDto) {
+    let data = dto
+
+    if (dto.password) {
+      data = { ...dto, password: await hash(dto.password) }
+    }
+
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data,
+      select: {
+        name: true,
+        email: true,
+      },
+    })
+  }
+
+  async getProfile(id: string) {
+    const profile = await this.getById(id)
+
+    const totalTasks = profile.tasks.length
+    // method must be written in task service
+    const completedTasks = await this.prisma.task.count({
+      where: {
+        userId: id,
+        isCompleted: true,
+      },
+    })
+
+    const todayStart = startOfDay(new Date())
+    const weekStart = startOfDay(subDays(new Date(), 7))
+
+    // method must be written in task service
+    const todayTasks = await this.prisma.task.count({
+      where: {
+        userId: id,
+        createdAt: {
+          gte: todayStart.toISOString(),
+        },
+      },
+    })
+
+    // method must be written in task service
+    const weekTasks = await this.prisma.task.count({
+      where: {
+        userId: id,
+        createdAt: {
+          gte: weekStart.toISOString(),
+        },
+      },
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...rest } = profile
+
+    return {
+      user: rest,
+      statistics: [
+        { label: 'Total', value: totalTasks },
+        { label: 'Completed tasks', value: completedTasks },
+        { label: 'Today tasks', value: todayTasks },
+        { label: 'Week tasks', value: weekTasks },
+      ],
+    }
   }
 }
