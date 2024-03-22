da8a8bce4512225161802ba3d3f343d2eda86433
zim89 <121241841+zim89@users.noreply.github.com>
2024-03-22 14:33:54 +0200

    tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
-import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module'
+import { TaskModule } from './task/task.module'
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, TaskModule],
 })
 export class AppModule {}
